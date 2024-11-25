8c7f23246decae50838bcdc6884a482f98872937
malfinnr <malfinnr98@gmail.com>
2024-11-25 22:46:57 +0700

    feat: day 1 layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,12 +7,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      {/* Main 1 */}
       <div
         className="bg-cover bg-center h-screen"
         style={{ backgroundImage: "url('https://placehold.co/1920x1080')" }}
       >
+        <Navbar />
         <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
           <div className="text-center text-white px-4">
             <h1 className="text-5xl font-bold mb-6">HELLO THERE</h1>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,60 +1,61 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-    const router = useRouter();
-    
-    const handleLogout = () => {
-        
-        router.push("/login");
-    }
+  const router = useRouter();
 
-    return (
-        <div className="navbar bg-base-100">
-            <img
-                src="images/PensiunPlan.png"
-                alt="PensiunPlan"
-                style={{ width: "50px", height: "auto" }}
-            />
+  const handleLogout = () => {
+    router.push("/login");
+  };
 
-            {/* Navigasi Halaman */}
-            <div className="flex-1 flex justify-center space-x-4">
-                <Link href='/' className="btn btn-ghost text-xl">Home</Link>
-                <Link href='/insert' className="btn btn-ghost text-xl">Dashboard</Link>
+  return (
+    <div className="navbar sticky top-0">
+      <img
+        src="images/PensiunPlan.png"
+        alt="PensiunPlan"
+        style={{ width: "50px", height: "auto" }}
+      />
+      <div className="flex-1 flex justify-center space-x-4">
+        <Link href="/" className="btn btn-ghost text-xl">
+          Home
+        </Link>
+        <Link href="/insert" className="btn btn-ghost text-xl">
+          Dashboard
+        </Link>
+      </div>
+
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
             </div>
-
-            <div className="flex-none gap-2">
-                <div className="dropdown dropdown-end">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar"
-                    >
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
-                        </div>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                    >
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a onClick={handleLogout}>Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a onClick={handleLogout}>Logout</a>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
