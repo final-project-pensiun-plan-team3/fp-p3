daa26b8443d951071c4e54cec9410438dcd363a6
Prima <widianiprima@gmail.com>
2024-11-25 17:14:26 +0700

    feat(server): add get data function for Saving model

--- client/src/db/models/savings.ts
+++ client/src/db/models/savings.ts
@@ -1,5 +1,15 @@
+import { HttpError } from "@/lib/errorhandler";
 import { database } from "../config";
+import { ObjectId } from "mongodb";
 
-export class Saving{
-    static db = database.collection("Savings")
+export class Saving {
+	static db = database.collection("Savings");
+
+	static async getData() {
+		const UserId = new ObjectId("6744362e16f24b8ddf424622");
+		if (!UserId) {
+			throw new HttpError("User Id is required", 401);
+		}
+		return await this.db.find({ UserId }).toArray();
+	}
 }
