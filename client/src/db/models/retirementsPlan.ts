85fc88cac09d0d30bbf1037d52356ff4b6beca8b
Brandon Vincentius <brandonvincentius54321@gmail.com>
2024-11-27 11:52:22 +0700

    feat:make feature to update total savings

--- client/src/db/models/retirementsPlan.ts
+++ client/src/db/models/retirementsPlan.ts
@@ -10,17 +10,41 @@ export class RetirementPlan {
     // console.log("🚀 ~ RetirementPlan ~ Create ~ insertedData:", insertedData)
     await this.db.insertOne(insertedData);
     console.log(indonesianDate());
-    
   }
 
-  static async getData(UserId:string) {
+  static async getData(UserId: string) {
     if (!UserId) {
       throw new HttpError("User Id is required", 401);
     }
 
-    const data = await this.db.findOne({UserId:new ObjectId(UserId)});
+    const data = await this.db.findOne({ UserId: new ObjectId(UserId) });
     // console.log("🚀 ~ RetirementPlan ~ getData ~ data:", data)
-    
+
     return data;
+  }
+
+  static async updateSavings(additionalSaving: number, UserId: string) {
+    if (!UserId) {
+      throw new HttpError("User Id is required", 401);
+    }
+
+    const data = await this.db.findOne({ UserId: new ObjectId(UserId) });
+    if (!data) {
+      throw new HttpError("Data Not Found", 404);
+    }
+
+    const result = await this.db.updateOne(
+      { UserId: new ObjectId(UserId) },
+      { $inc: { totalSavings: additionalSaving } }
+    );
+
+    if (result.modifiedCount === 1) {
+      const updatedData = await this.db.findOne({
+        UserId: new ObjectId(UserId),
+      });
+      return updatedData;
+    } else {
+      throw new HttpError("Failed to update savings", 500);
+    }
   }
 }
