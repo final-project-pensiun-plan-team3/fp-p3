c5637ca2f434821a300bcf899259ade31f90a035
Joses-Steven-Tarigan <steventarigan82@gmail.com>
2024-11-28 20:46:21 +0700

    Feat: add protect API and page with middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,5 +1,54 @@
-import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { HttpError, handleError } from "./lib/errorhandler";
+import * as jose from "jose";
 
-export default function Middleware() {
-  return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  try {
+    console.log("Masuk middleware nih ...", request.nextUrl.pathname);
+
+    const authCookie = cookies().get("Authorization");
+    console.log(authCookie);
+
+    if (authCookie && request.nextUrl.pathname === "/login") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    if (!authCookie && request.nextUrl.pathname === "/login") {
+      return NextResponse.next();
+    }
+
+    if (!authCookie) {
+      throw new HttpError("Invalid Token", 401);
+    }
+
+    const token = authCookie.value;
+    if (!token) {
+      throw new HttpError("Invalid Token", 401);
+    }
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET as string);
+
+    const result = await jose.jwtVerify<{ UserId: string }>(token, secret);
+    console.log(result.payload);
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-UserId", result.payload.UserId);
+
+    return NextResponse.next({
+      request: {
+        // New request headers
+        headers: requestHeaders,
+      },
+    });
+
+    // return NextResponse.next();
+  } catch (error) {
+    console.log(error);
+
+    return handleError(error);
+  }
 }
+
+export const config = {
+  matcher: ["/onboarding", "/apis/retirement", "/dashboard"],
+};
