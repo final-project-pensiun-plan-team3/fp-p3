4821435a7fc89df5c090111c4e860f0ab86bac70
Brandon Vincentius <brandonvincentius54321@gmail.com>
2024-12-04 16:13:56 +0700

    fix:add revalidate path

--- client/src/action.ts
+++ client/src/action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+// import axios from "axios";
 import { cookies } from "next/headers";
 
 export async function deleteCookies(names: string[]) {
@@ -11,4 +13,19 @@ export async function deleteCookies(names: string[]) {
 
 export async function setCookies(name:string,value:string) {
   cookies().set(name, value);
+}
+
+// export const fetchData = async () => {
+//   try {
+//     const response = await axios.get(
+//       `${process.env.NEXT_PUBLIC_BASE_URL}/apis/retirement`
+//     );
+//     return(response.data);
+//   } catch (error) {
+//     console.log("🚀 ~ fetchData ~ error:", error);
+//   }
+// };
+
+export async function revalidateBypath(path:string) {
+  revalidatePath(path)
 }
